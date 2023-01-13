0ec227f967dca48887884c81e5197dcbc523cad0
larg0c <largo.ctlo@tutanota.de>
2023-01-13 15:18:12 +0100

    UPDATE

--- popup/window-action.js
+++ popup/window-action.js
@@ -1,15 +1,46 @@
-const boutons=document.querySelectorAll(".yt_button");
-boutons.forEach(b => {b.addEventListener("click", function(){
-	$.ajax({
-		url:'test.php',
-		type: 'POST',
-		data: {amount: this.value},
-		success: function (json_data) {
-			window.location.replace(json_data);
-		},
-		error: function() {
-			console.log("data not found");
-		}
+var piped_action = true;
+export {piped_action};
+
+
+// Récupère le bouton
+var toggleButton = document.getElementById("piped_onoff");
+
+// Ajoute un écouteur d'événement pour détecter un clic sur le bouton
+toggleButton.addEventListener("click", function () {
+	// Récupère le corps de la page
+	var body = document.body;
+	// Inverse la valeur de la variable "mode"
+	piped_action = !piped_action;
+	/*
+	// Vérifie si "mode" est true 
+	if (pieped_action) {
+		// Ajoute la classe "dark-mode"
+		body.classList.add("dark-mode");
+		// Change le texte du bouton pour désactiver le mode sombre
+		toggleButton.innerHTML = "Désactiver le mode sombre";
+	} else {
+		// Sinon, supprime la classe "dark-mode"
+		body.classList.remove("dark-mode");
+		// Change le texte du bouton pour activer le mode sombre
+		toggleButton.innerHTML = "Activer le mode sombre";
+	}*/
+});
+
+
+
+const boutons = document.querySelectorAll(".yt_button");
+boutons.forEach(b => {
+	b.addEventListener("click", function () {
+		$.ajax({
+			url: 'test.php',
+			type: 'POST',
+			data: { amount: this.value },
+			success: function (json_data) {
+				window.location.replace(json_data);
+			},
+			error: function () {
+				console.log("data not found");
+			}
+		});
 	});
-}); 
 })  
--- redirect.js
+++ redirect.js
@@ -1,15 +1,50 @@
+// Récupère la valeur du cookie "piped_mode"
+var pipedMode = getCookie("piped_mode");
+  
+// Vérifie si la valeur du cookie est "true"
+if (pipedMode === "true") {
+    var parser = document.createElement('a');
+    parser.href = window.location.href;
 
-var parser = document.createElement('a');
-parser.href = window.location.href;
+    parser.protocol; // => "http:"
+    parser.host;     // => "example.com:3000"
+    parser.hostname; // => "example.com"
+    parser.port;     // => "3000"
+    let path = parser.pathname; // => "/pathname/"
+    let search = parser.search;   // => "?search=test"
+    let hash = parser.hash;     // => "#hash"
+    let url = 'https://piped.video';
 
-parser.protocol; // => "http:"
-parser.host;     // => "example.com:3000"
-parser.hostname; // => "example.com"
-parser.port;     // => "3000"
-let path = parser.pathname; // => "/pathname/"
-let search = parser.search;   // => "?search=test"
-let hash = parser.hash;     // => "#hash"
-let url = 'https://piped.video';
+    let new_url = url.concat(path, search, hash);
+    location.href = new_url;
+  console.log("Le mode piped est activé");
+} else {
+  // Sinon, le mode "piped" est désactivé
+  console.log("Le mode piped est désactivé");
+}
 
-let new_url = url.concat(path, search, hash);
-location.href = new_url;
+// Fonction pour récupérer la valeur d'un cookie
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+if (mode === true) {
+    var parser = document.createElement('a');
+    parser.href = window.location.href;
+
+    parser.protocol; // => "http:"
+    parser.host;     // => "example.com:3000"
+    parser.hostname; // => "example.com"
+    parser.port;     // => "3000"
+    let path = parser.pathname; // => "/pathname/"
+    let search = parser.search;   // => "?search=test"
+    let hash = parser.hash;     // => "#hash"
+    let url = 'https://piped.video';
+
+    let new_url = url.concat(path, search, hash);
+    location.href = new_url;
+}
+
+
